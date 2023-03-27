db487e9d3fff0bb8c6ef74aea6b7e1ad7bdddcea
Krzysztof Matyga <matyga.krzysztof@gmail.com>
2023-03-27 13:14:17 +0200

    todo list added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from './features/api/apiSlice';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -11,9 +11,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ApiProvider api={apiSlice}>
       <App />
-    </Provider>
+    </ApiProvider>
   </React.StrictMode>
 );
 
